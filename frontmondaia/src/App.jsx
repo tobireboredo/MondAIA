e1219af0d66be4800a2ad2a2e8b40e297c7a75e1
tobireboredo <tobiasreboredo15@gmail.com>
2025-11-29 16:08:35 -0300

    no funciona el back x alguna razon ???¿

--- frontmondaia/src/App.jsx
+++ frontmondaia/src/App.jsx
@@ -1,25 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Login from "./Login";
 import Register from "./Register";
-import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Router>
+    <>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["WWW-Authenticate"],
 )
 
 @app.middleware("http")
